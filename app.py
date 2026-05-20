45b98a04112786e33a9398bbdbc3a9ae4394376a
jmossVB <98797411+jmossVB@users.noreply.github.com>
2026-05-19 22:13:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,5 @@ st.sidebar.title("Parametros")
 st.write("Elaborado por jmoss")
 
 st.sidebar.image("DMC.png")
+
+sesion = st.sidebar.selectbox("Seleccione una sesión", ["Sesión 1","Sesión 2","Sesión 3","Sesión 4"] )
