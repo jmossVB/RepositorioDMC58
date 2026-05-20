cfbd0488f1d7865433059558e22a6d1f32a1c2cc
jmossVB <98797411+jmossVB@users.noreply.github.com>
2026-05-19 22:14:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,3 +12,14 @@ sesion = st.sidebar.selectbox("Seleccione una sesión", ["Sesión 1","Sesión 2"
 if sesion == "Sesión 1":
   st.write("Bienvenido la sesión 1")
   st.image("Python_logo.png" )
+  
+elif sesion == "Sesión 2":
+  st.write("Bienvenido la sesión 2")
+
+  precio = st.number_input("Ingrese el precio del producto", min_value = 0 , max_value = 5000 , value = 1200)
+  descuento = st.number_input("Ingrese el descuento del producto del 0 al 100% ", min_value = 0 , max_value = 100 )
+
+  precio_final_producto = precio - (precio*(descuento/100))
+
+  st.write("El precio final del producto es: ", precio_final_producto  )
+
